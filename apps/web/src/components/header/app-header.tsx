71a9a48a9b693c77fa4a932e3479d143cba7f003
Matheus Agostinho <contact@matheuspa.com>
2025-12-09 12:27:53 -0300

    fix(app-header): adjust header layout for better alignment

--- apps/web/src/components/header/app-header.tsx
+++ apps/web/src/components/header/app-header.tsx
@@ -1,9 +1,17 @@
+import { Link } from '@tanstack/react-router'
 import { Anchor } from '../anchor'
 import { NavUser } from './nav-user'
 
 export function AppHeader() {
   return (
-    <header className="container flex justify-end items-center my-4">
+    <header className="container flex justify-between items-center my-4">
+      <Link
+        to="/"
+        className="hover:scale-95 active:scale-110 transition-all duration-200 ease-in-out flex flex-row gap-2 items-center"
+      >
+        <img src="/favicon.svg" alt="PZ Packs" className="h-16" />
+        <h1 className="font-bold text-2xl">PZ Packs</h1>
+      </Link>
       <nav className="flex flex-row gap-3 items-center">
         <Anchor href="/">Home</Anchor>
         <Anchor href="/modpacks">My Modpacks</Anchor>
