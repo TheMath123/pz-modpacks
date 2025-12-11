d10d8b15ace0bcb5f688db8e61ba278c49423dab
Matheus Agostinho <contact@matheuspa.com>
2025-12-10 22:43:02 -0300

    fix(home-page): correct spelling of "Welcome" in the homepage header

--- apps/web/src/pages/-components/home-page.tsx
+++ apps/web/src/pages/-components/home-page.tsx
@@ -4,7 +4,7 @@ export function HomePage() {
   return (
     <main className="container flex flex-col gap-8 p-4 md:p-8">
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold">Wellcome to PZ Modpacks!</h1>
+        <h1 className="text-2xl font-bold">Welcome to PZ Modpacks!</h1>
         <h2 className="text-lg font-medium ">
           Your gateway to the best Project Zomboid modpacks.
         </h2>
