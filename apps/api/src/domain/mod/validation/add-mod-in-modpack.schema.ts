106563b2b57a16d195f77b7dfcf4f9a60c329916
Matheus Agostinho <contact@matheuspa.com>
2025-12-14 09:50:26 -0300

    fix(validation): update addModInModpackSchema to remove number validation for modAtribute

--- apps/api/src/domain/mod/validation/add-mod-in-modpack.schema.ts
+++ apps/api/src/domain/mod/validation/add-mod-in-modpack.schema.ts
@@ -4,7 +4,6 @@ export const addModInModpackSchema = z.object({
   modAtribute: z
     .string()
     .min(1, { error: 'Mod attribute is required' })
-    .or(z.number({ error: 'Mod attribute must be a number' }))
     .or(z.url({ error: 'Mod attribute must be a valid URL' })),
 })
 
