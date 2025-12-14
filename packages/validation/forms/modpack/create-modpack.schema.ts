d18c7b20f4c888113c3471db348f84df9bd061f4
Matheus Agostinho <contact@matheuspa.com>
2025-12-14 09:49:31 -0300

    fix(schema): update description length validation to allow up to 512 characters

--- packages/validation/forms/modpack/create-modpack.schema.ts
+++ packages/validation/forms/modpack/create-modpack.schema.ts
@@ -7,7 +7,7 @@ export const createModpackFormSchema = z.object({
     .max(50, { error: 'Name must be at most 50 characters long' }),
   description: z
     .string({ error: 'Description must be a string' })
-    .max(500, { error: 'Description must be at most 500 characters long' })
+    .max(512, { error: 'Description must be at most 512 characters long' })
     .optional(),
   avatarUrl: z
     .url({ error: 'Invalid URL format for avatar' })
